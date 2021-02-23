a012c92073ab749898f991751715100339629163
Ali Almohammad <ali@iotechsys.com>
2021-02-23 13:22:49 +0000

    Added apt repo for SaltStack

--- Ubuntu_20_04/bootstrap.sh
+++ Ubuntu_20_04/bootstrap.sh
@@ -12,4 +12,7 @@ usermod -aG docker vagrant
 # Install docker-compose
 apt-get install -y docker-compose
 
-
+# Added to be able to install SaltStack
+wget -O - https://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+echo "deb http://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee -a /etc/apt/sources.list.d/saltstack.list
+sudo apt update
